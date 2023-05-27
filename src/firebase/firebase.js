65d63c9e391fea40cba72eddcc3a07ad8764110f
oh-wilks <wilcoxivan@hotmail.com>
2023-05-27 09:17:05 -0700

    firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCO9obpH46wGr5vDDHp35cQ1ba_Oy5t-Ks",
+  authDomain: "peliculas-app-88748.firebaseapp.com",
+  projectId: "peliculas-app-88748",
+  storageBucket: "peliculas-app-88748.appspot.com",
+  messagingSenderId: "392962763550",
+  appId: "1:392962763550:web:3008b61fdbb90aa0c1b70f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
